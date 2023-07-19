c743d7fc514496bce062c107193ed86cd99cd59c
astongarden <9812choi@naver.com>
2023-07-19 15:20:49 +0900

    save

--- making_file/2dof_2D.py
+++ making_file/2dof_2D.py
@@ -4,7 +4,6 @@ import math
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
 import time
-import graph_heatmap
 import numpy as np
 
 # color
@@ -89,6 +88,39 @@ def run():
     with open(file_path, "a") as file:
         file.write(f"\ncalculation time is : {end - start :.5f} sec")
 
+def C_space():
+    file_path = "/home/jeongil/collision/making_file/2dof_2D_graph_data.txt"
+
+    x_values = []
+    y_values = []
+    z_values = []
+
+    with open(file_path, "r") as file:
+        for line in file:
+            data = line.strip().split(",")
+            x = float(data[0])
+            y = float(data[1])
+            z = float(data[2])
+            x_values.append(x)
+            y_values.append(y)
+            z_values.append(z)
+
+    x_unique = np.sort(np.unique(x_values))
+    y_unique = np.sort(np.unique(y_values))
+    z_array = np.zeros((len(y_unique), len(x_unique)))
+
+    for x, y, z in zip(x_values, y_values, z_values):
+        x_index = np.where(x_unique == x)[0][0]
+        y_index = np.where(y_unique == y)[0][0]
+        z_array[y_index, x_index] = z
+
+    plt.imshow(z_array, cmap='Pastel1', origin='upper', extent=[x_unique[0], x_unique[-1], y_unique[0], y_unique[-1]])
+
+    plt.xlabel("joint 1 angle(q1, degrees)")
+    plt.ylabel("joint 2 angle(q2, degrees)")
+
+    plt.title("C-space")
+    plt.show()
 
 def pairs(points):
     for i, j in enumerate(range(-1, len(points) - 1)):
@@ -111,8 +143,11 @@ def line(start, end, color=BLACK, camera=(0, 0)):
 def add(p1, p2):
     return p1[0] + p2[0], p1[1] + p2[1]
 
-# run code 
 
+
+
+# run code 
 if __name__ == '__main__':
     run()
+    C_space()
 
