d20482af5fa53522caa46740e7351dba8b62340a
astongarden <9812choi@naver.com>
2023-07-19 10:50:46 +0900

    save

--- making_file/make.v2.py
+++ making_file/make.v2.py
@@ -17,71 +17,69 @@ RED   = (255,   0,   0)
 robot_link1 = 200
 robot_link2 = 200
 robot_thickness = 20
-q1 = 0
-q2 = 2
-
-   
-
 
 def run():
-    # for q1 in range(0, 10):
-    #     for q2 in range(0, 10):
-     
-    link_1 = (
-        (robot_thickness*math.sin(q1),-robot_thickness*math.cos(q1)),
-        (-robot_thickness*math.sin(q1),robot_thickness*math.cos(q1)),
-        (-robot_thickness*math.sin(q1)+robot_link1*math.cos(q1),robot_thickness*math.cos(q1)+robot_link1*math.sin(q1)),
-        (robot_thickness*math.sin(q1)+robot_link1*math.cos(q1),-robot_thickness*math.cos(q1)+robot_link1*math.sin(q1))
-        )
+    for q1_rad in range(0, 10):
+        for q2_rad in range(0, 10):
+            
+            q1 = math.radians(q1_rad) 
+            q2 = math.radians(q2_rad) 
 
-    link_2 = (
-        (robot_link1*math.cos(q1)+robot_thickness*math.sin(q2),robot_link1*math.sin(q1)-robot_thickness*math.cos(q2)),
-        (robot_link1*math.cos(q1)-robot_thickness*math.sin(q2),robot_link1*math.sin(q1)+robot_thickness*math.cos(q2)),
-        (robot_link1*math.cos(q1)-robot_thickness*math.sin(q2)+robot_link2*math.cos(q2),robot_link1*math.sin(q1)+robot_thickness*math.cos(q2)+robot_link2*math.sin(q2)),
-        (robot_link1*math.cos(q1)+robot_thickness*math.sin(q2)+robot_link2*math.cos(q2),robot_link1*math.sin(q1)-robot_thickness*math.cos(q2)+robot_link2*math.sin(q2))
-        )
+            link_1 = (
+                (robot_thickness*math.sin(q1),-robot_thickness*math.cos(q1)),
+                (-robot_thickness*math.sin(q1),robot_thickness*math.cos(q1)),
+                (-robot_thickness*math.sin(q1)+robot_link1*math.cos(q1),robot_thickness*math.cos(q1)+robot_link1*math.sin(q1)),
+                (robot_thickness*math.sin(q1)+robot_link1*math.cos(q1),-robot_thickness*math.cos(q1)+robot_link1*math.sin(q1))
+                )
+
+            link_2 = (
+                (robot_link1*math.cos(q1)+robot_thickness*math.sin(q2),robot_link1*math.sin(q1)-robot_thickness*math.cos(q2)),
+                (robot_link1*math.cos(q1)-robot_thickness*math.sin(q2),robot_link1*math.sin(q1)+robot_thickness*math.cos(q2)),
+                (robot_link1*math.cos(q1)-robot_thickness*math.sin(q2)+robot_link2*math.cos(q2),robot_link1*math.sin(q1)+robot_thickness*math.cos(q2)+robot_link2*math.sin(q2)),
+                (robot_link1*math.cos(q1)+robot_thickness*math.sin(q2)+robot_link2*math.cos(q2),robot_link1*math.sin(q1)-robot_thickness*math.cos(q2)+robot_link2*math.sin(q2))
+                )
             
 
 
-    obstacle = ((300, 0), 20)
+            obstacle = ((300, 0), 20)
 
-    collide_1 = gjk.collidePolyCircle(link_1, obstacle)
-    polygon(link_1)
-    circle(obstacle)
-    collide_2 = gjk.collidePolyCircle(link_2, obstacle)
-    polygon(link_2)
-    circle(obstacle)
+            collide_1 = gjk.collidePolyCircle(link_1, obstacle)
+            polygon(link_1)
+            circle(obstacle)
+            collide_2 = gjk.collidePolyCircle(link_2, obstacle)
+            polygon(link_2)
+            circle(obstacle)
 
 
     # make graph
 
-    fig, ax = plt.subplots()
+            fig, ax = plt.subplots()
 
-    link1_patch = patches.Polygon(link_1, edgecolor='blue', facecolor='blue')
-    ax.add_patch(link1_patch)
-    
-    link2_patch = patches.Polygon(link_2, edgecolor='red', facecolor='red')
-    ax.add_patch(link2_patch)
+            link1_patch = patches.Polygon(link_1, edgecolor='blue', facecolor='blue')
+            ax.add_patch(link1_patch)
+            
+            link2_patch = patches.Polygon(link_2, edgecolor='red', facecolor='red')
+            ax.add_patch(link2_patch)
 
-    obstacle_patch = patches.Circle(obstacle[0], obstacle[1], edgecolor='black', facecolor='black')
-    ax.add_patch(obstacle_patch)
+            obstacle_patch = patches.Circle(obstacle[0], obstacle[1], edgecolor='black', facecolor='black')
+            ax.add_patch(obstacle_patch)
 
-    ax.set_xlim(-400, 400)
-    ax.set_ylim(-400, 400)
-    
-    plt.show()
+            ax.set_xlim(-400, 400)
+            ax.set_ylim(-400, 400)
+            
+    # plt.show()
 
 
     # print result
 
-    print('True' if (collide_1 or collide_2) else 'fail')
+            # print('True' if (collide_1 or collide_2) else 'fail')
 
-    file_path = "/home/jeongil/collision/making_file/result.txt"
-    
-    with open(file_path, "w") as file:
-        file.write(f"q1 : {q1}  ")
-        file.write(f"q2 : {q2}  ")
-        file.write(f"collision resutl : {'True' if (collide_1 or collide_2) else 'fail'}\n")
+            file_path = "/home/jeongil/collision/making_file/result.txt"
+            
+            with open(file_path, "a") as file:
+                file.write(f"q1 : {q1_rad}  ")
+                file.write(f"q2 : {q2_rad}  ")
+                file.write(f"collision resutl : {'True' if (collide_1 or collide_2) else 'fail'}\n")
 
 
 
@@ -114,14 +112,3 @@ def add(p1, p2):
 
 if __name__ == '__main__':
     run()
-
-# input = "fds"
-# output = "asdf"
-
-# data = "input : " + input + "   collision result : " + output + "\n"
-
-# file_path = "/home/jeongil/collision/making_file/result.txt"
-
-# with open(file_path, "w") as file:
-#     file.write(data)
-
