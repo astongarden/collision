b172d30f2771cd0101b9ce1ff6bdc222f9d8cf1a
astongarden <9812choi@naver.com>
2023-07-25 13:34:53 +0900

    save

---
+++ making_file/test.py
@@ -0,0 +1,13 @@
+
+
+
+
+def load_data(file_path):
+    data = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            angle1, angle2, collision = line.strip().split(',')
+            data.append([float(angle1), float(angle2), int(collision)])
+    return np.array(data)
+
+
