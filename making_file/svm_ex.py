54057ab727b729312a34a35397353e41ab232872
astongarden <9812choi@naver.com>
2023-07-27 17:53:57 +0900

    save

--- making_file/svm_ex.py
+++ making_file/svm_ex.py
@@ -32,7 +32,7 @@ def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
         
 
 np.random.seed(1)
-X_xor = np.random.randn(200, 2)
+X_xor = np.random.randn(10, 2)
 y_xor = np.logical_xor(X_xor[:, 0] > 0,
                        X_xor[:, 1] > 0)
 y_xor = np.where(y_xor, 1, -1)
